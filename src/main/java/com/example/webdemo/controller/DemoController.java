43e022f774928d694f9308f025b4ea2d2a06c5d7
FinalZZSSYY <980037227@qq.com>
2019-10-20 19:41:10 +0800

    DateUtil 日期工具类

--- src/main/java/com/example/webdemo/controller/DemoController.java
+++ src/main/java/com/example/webdemo/controller/DemoController.java
@@ -20,15 +20,21 @@ package com.example.webdemo.controller;//
 //        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //                      Buddha Bless, No Bug !
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: Zhou ShiYang
  * @Description:
  * @Date:Create：in 2019/10/20 19:26
  */
-@Controller
+@RestController
 @RequestMapping("/DemoController")
 public class DemoController {
+
+    @GetMapping("/")
+    public String index(){
+        return "你好";
+    }
 }
